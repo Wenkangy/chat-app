e0c254a014f0377edb29c763ed562d34302c2234
Wenkang <166893688+Wenkangy@users.noreply.github.com.>
2024-06-11 17:24:48 -0700

    restore my old server.js

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,34 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const userRoutes = require("./routes/userRoutes");
-const chatRoutes = require("./routes/chatRoutes");
-const messageRoutes = require("./routes/messageRoutes");
+const userRoutes = require("./routes/userRoutes")
+const chatRoutes= require("./routes/chatRoutes")
+const messageRoutes = require("./routes/messageRoutes")
 const path = require("path");
 
 const app = express();
 dotenv.config();
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
-app.use(cors({
-  origin: "https://chat-app-9jrw.onrender.com",
-  credentials: true,
-}));
+app.use(cors());
 
-app.options("*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "https://chat-app-9jrw.onrender.com");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,OPTIONS");
+app.options("*", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "GET,PUT,POST,OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
     "Authorization, Content-Length, X-Requested-With"
   );
-  res.sendStatus(200);
+  res.send(200);
 });
 
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
@@ -37,11 +36,16 @@ app.use((req, res, next) => {
   next();
 });
 
+
+
+
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-// Deployment
+
+//deployment
+
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
@@ -52,55 +56,78 @@ if (process.env.NODE_ENV === "production") {
   );
 } else {
   app.get("/", (req, res) => {
-    res.send("API is running..");
+    res.sendFile(__dirname + "/views/index.html");
+    res.send("api running");
   });
 }
 
-// Start server
-const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+//deployment
 
-// Socket.io setup
-const io = require("socket.io")(server, {
-  pingTimeout: 60000,
-  cors: {
-    origin: "https://chat-app-9jrw.onrender.com",
-    credentials: true,
+/*app.get("/auth/userName/:userName/pswd/:pswd", (req, res) => {
+  if (verify_login(req.params.userName, req.params.pswd)) {
+    var token = jwt.sign({ username: `${req.params.userName}` }, "secret", {
+      expiresIn: 120,
+    });
+    res.send(token);
+  }
+  res.send(req.params);
+});
+
+app.all("*", (req, res) => {
+  res.send(req.params);
+});
+*/
+
+const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+const io = require('socket.io')(server,{
+  pingTimeout:60000,
+  cors:{
+    origin: "http://localhost:3000"
   },
+
 });
 
 io.on("connection", (socket) => {
-  console.log("Connected to socket.io");
-
-  socket.on("setup", (userData) => {
+  //console.log("connected to socket.io")
+  
+  socket.on("setup", (userData)=>{
     socket.join(userData._id);
+    
     socket.emit("connected");
-    console.log(`User ${userData._id} connected`);
   });
 
   socket.on("join chat", (room) => {
     socket.join(room);
-    console.log(`User joined room: ${room}`);
+    console.log("User Joined Room: " + room);
   });
 
   socket.on("typing", (room) => socket.in(room).emit("typing"));
   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
-  socket.on("new message", (newMessageReceived) => {
-    const chat = newMessageReceived.chat;
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    // broadcasts the received message to all users in the chat room, except the sender
     if (!chat.users) return console.log("chat.users not defined");
 
     chat.users.forEach((user) => {
-      if (user._id === newMessageReceived.sender._id) return;
-      socket.in(user._id).emit("message received", newMessageReceived);
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
     });
   });
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
-
-  socket.off("setup", (userData) => {
-    console.log(`User ${userData._id} disconnected`);
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
     socket.leave(userData._id);
   });
+
+
 });
+/*function verify_login(userName, pswd) {
+  if (userName == "kufooloo" && pswd == "secret") {
+    return true;
+  }
+  return false;
+}
+*/
